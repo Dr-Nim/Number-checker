3dc7da65203019a3c48e5b855aa8bd5f05713ac8
Akshat Raj <48322760+Akshat-Raj@users.noreply.github.com>
2021-10-03 10:48:14 +0530

    Update NumberChecker.py

--- NumberChecker.py
+++ NumberChecker.py
@@ -1,10 +1,11 @@
-n = int input('Enter a number')
+n = int(input('Enter a number'))
 if n%2 == 0:
   print('The number is even')
 else:
   print('The number is odd')
 
   
+
   
 def isPrime(n):
     if n <= 1 :

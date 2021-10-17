5c13d712eae4b04c66042da7517256d9c9d9f769
Dr-Nim <90766170+Dr-Nim@users.noreply.github.com>
2021-10-17 13:54:36 +0530

    Update prime_check.py

--- prime_check.py
+++ prime_check.py
@@ -1,9 +1,16 @@
-def isPrime(n):
-    if n <= 1 :
-        return False
+def isPrime(n):  
+    # Checking that given number is more than 1  
+    if n > 1:  
+        # Iterating over the given number with for loop  
+        for j in range(2, int(n/2) + 1):  
+            # If the given number is divisible or not  
+            if (a % j) == 0:  
+                print("Composite number")  
+                break  
+        # Else it is a prime number  
+        else:  
+            print("Prime number")  
+    # If the given number is 1  
+    else:  
+        print("Composite number")
  
-    for i in range(2, n):
-        if n % i == 0:
-            return False
- 
-    return True
